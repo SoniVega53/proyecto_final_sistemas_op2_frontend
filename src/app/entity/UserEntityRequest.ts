f260bb7b8340286cc95bc5278782e53cc528d9d2
SoniVega53 <svegam@miumg.edu.gt>
2025-06-11 17:00:03 -0600

    cambio null data

--- src/app/entity/UserEntityRequest.ts
+++ src/app/entity/UserEntityRequest.ts
@@ -1,28 +1,28 @@
 export class UserEntityRequest {
-  id?: number;
-  name?: string;
-  username?: string;
-  lastname?: string;
-  email?: string;
-  password?: string;
-  passwordChange?: string;
-  rol?: string;
-  specialty?: string;
-  phone?: string;
-  id_doctor?: number;
-  id_paciente?: number;
+  id?: number = 0;
+  name?: string = '';
+  username?: string = '';
+  lastname?: string = '';
+  email?: string = '';
+  password?: string = '';
+  passwordChange?: string = '';
+  rol?: string = '';
+  specialty?: string = '';
+  phone?: string = '';
+  id_doctor?: number = 0;
+  id_paciente?: number = 0;
 }
 
 export class Doctor {
-  id?: number;
-  name?: string;
-  lastname?: string;
-  specialty?: string;
+  id?: number = 0;
+  name?: string = '';
+  lastname?: string = '';
+  specialty?: string = '';
 }
 
 export class Paciente {
-  id?: number;
-  name?: string;
-  lastname?: string;
-  phone?: string;
+  id?: number = 0;
+  name?: string = '';
+  lastname?: string = '';
+  phone?: string = '';
 }
